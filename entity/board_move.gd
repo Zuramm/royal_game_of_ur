195edbf51e6a88b2f3bfd15883b33f9aaf20dae1
Zuramm <opheder@gmail.com>
2025-06-25 22:00:45 +0200

    improve board move line rendering

--- entity/board_move.gd
+++ entity/board_move.gd
@@ -18,8 +18,9 @@ signal selected
 func _ready() -> void:
 	if positions == null or positions.is_empty() \
 	or positions.size() < 2:
+		push_error("Invalid positions: ", positions)
 		return
-	
+
 	_collision_node.shape = collision_shape
 	_collision_node.position = collision_position
 	_highlight_node.width = collision_shape.size.x
@@ -53,45 +54,84 @@ func _input_event(_camera: Camera3D, event: InputEvent, _position_: Vector3, _no
 		selected.emit()
 
 
+func _sanitized_positions() -> Array[Vector3]:
+	var sanitized: Array[Vector3] = []
+
+	for p in positions:
+		if sanitized.size() == 0:
+			sanitized.push_back(p)
+		elif sanitized.size() == 1:
+			var p1 := sanitized[sanitized.size() - 1]
+			var p2 := p
+			if p1.distance_to(p2) > 0.000001:
+				sanitized.push_back(p)
+		else:
+			var p1 := sanitized[sanitized.size() - 2]
+			var p2 := sanitized[sanitized.size() - 1]
+			var p3 := p
+			var v1 := (p2 - p1).normalized()
+			var v2 := (p3 - p2).normalized()
+			if p1.distance_to(p2) > 0.000001 and v1.dot(v2) < 0.999999:
+				sanitized.push_back(p)
+
+	return sanitized
+
 func update_path() -> void:
+	if positions == null or positions.size() < 2:
+		push_error("Invalid positions: ", positions)
+		return
+
+	var sanitized: Array[Vector3] = _sanitized_positions()
+
+	if sanitized.size() < 2:
+		push_error("Invalid sanitized positions: ", sanitized)
+		return
+
 	var mesh := _mesh_node.mesh as ImmediateMesh
 	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
+	
+	var p1: Vector3
+	var p2: Vector3
+	var p3: Vector3
+	var v1: Vector3
+	var v2: Vector3
+	var w1: Vector3
+	var w2: Vector3
+	var left: Vector3
+	var right: Vector3
 
-	if positions.size() >= 2:
-		var p1 := positions[0]
-		var p2 := positions[1]
-		var v := (p2 - p1).normalized()
-		var w := v.rotated(Vector3.UP, PI / 2) * thickness / 2
-		var left := p1 - w
-		var right := p1 + w
-		mesh.surface_add_vertex(left)
-		mesh.surface_add_vertex(right)
+	p1 = positions[0]
+	p2 = positions[1]
+	v1 = (p2 - p1).normalized()
+	w1 = v1.rotated(Vector3.UP, PI / 2) * thickness / 2
+	left = p1 - w1
+	right = p1 + w1
+	mesh.surface_add_vertex(left)
+	mesh.surface_add_vertex(right)
 
 	for i in range(0, positions.size() - 1):
-		var p1 := positions[i - 1]
-		var p2 := positions[i]
-		var p3 := positions[i + 1]
-		var v1 := (p2 - p1).normalized()
-		var v2 := (p3 - p2).normalized()
-		var w1 := v1.rotated(Vector3.UP, PI / 2) * thickness / 2
-		var w2 := v2.rotated(Vector3.UP, PI / 2) * thickness / 2
-		var left := _calculate_intersection(p1 - w1, v1, p2 - w2, v2)
-		var right := _calculate_intersection(p1 + w1, v1, p2 + w2, v2)
+		p1 = positions[i - 1]
+		p2 = positions[i]
+		p3 = positions[i + 1]
+		v1 = (p2 - p1).normalized()
+		v2 = (p3 - p2).normalized()
+		w1 = v1.rotated(Vector3.UP, PI / 2) * thickness / 2
+		w2 = v2.rotated(Vector3.UP, PI / 2) * thickness / 2
+		left = _calculate_intersection(p1 - w1, v1, p2 - w2, v2)
+		right = _calculate_intersection(p1 + w1, v1, p2 + w2, v2)
 		if left == Vector3.INF or right == Vector3.INF:
 			continue
 		mesh.surface_add_vertex(left)
 		mesh.surface_add_vertex(right)
 
-
-	if positions.size() >= 2:
-		var p1 := positions[positions.size() - 2]
-		var p2 := positions[positions.size() - 1]
-		var v := (p2 - p1).normalized()
-		var w := v.rotated(Vector3.UP, PI / 2) * thickness / 2
-		var left := p2 - w
-		var right := p2 + w
-		mesh.surface_add_vertex(left)
-		mesh.surface_add_vertex(right)
+	p1 = positions[positions.size() - 2]
+	p2 = positions[positions.size() - 1]
+	v1 = (p2 - p1).normalized()
+	w1 = v1.rotated(Vector3.UP, PI / 2) * thickness / 2
+	left = p2 - w1
+	right = p2 + w1
+	mesh.surface_add_vertex(left)
+	mesh.surface_add_vertex(right)
 
 	mesh.surface_end()
 
